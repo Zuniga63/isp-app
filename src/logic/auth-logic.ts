634d582fbee65ad7a9d83944dad14c377a4f1173
Andres Zuñiga <andres.zuniga.063@gmail.com>
2023-09-23 20:07:28 -0500

    refactor: update auth logic

---
+++ src/logic/auth-logic.ts
@@ -0,0 +1,55 @@
+import { authApi } from '@/services/auth.service';
+import { boxesApi } from '@/services/boxes.service';
+import axios from 'axios';
+import { getCookie, setCookie } from 'cookies-next';
+
+export function getTokenFromCookies() {
+  const cookieValue = getCookie('access_token');
+  if (typeof cookieValue !== 'string') return '';
+  return cookieValue;
+}
+
+/**
+ * Build a option for next-cookie
+ * @param duration Time in day of duration of cookie in the browser, default 1 day
+ * @returns
+ */
+export function createCookieOptions(duration = 1) {
+  return {
+    path: '/',
+    sameSite: true,
+    maxAge: 60 * 60 * 24 * duration,
+  };
+}
+
+/**
+ * Se encarga de actualizar todas las instancias de axios con el valor de la key
+ * @param token Llave de authorización
+ */
+export function setAuthTokens(token?: string) {
+  const authorization = token ? `Bearer ${token}` : token;
+  const axiosInstances = [axios, authApi, boxesApi];
+
+  axiosInstances.forEach(instance => {
+    instance.defaults.headers.common.Authorization = authorization;
+  });
+}
+
+/**
+ * Se encarga de guardar el token de authorización en las cookies y setearla en
+ * todas las instancias de axios creadas.
+ * @param token Token de authorización
+ */
+export const saveAuthToken = (token: string) => {
+  setCookie('access_token', token, createCookieOptions(1));
+  setAuthTokens(token);
+};
+
+/**
+ * Se encarga de eliminar el token de authorización de las cookies y actualizar todas las instancias
+ * de axios para remover la cabecera de authorización.
+ */
+export const clearAuthToken = () => {
+  setCookie('access_token', '', createCookieOptions(0));
+  setAuthTokens();
+};
--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -1,7 +1,5 @@
+import { clearAuthToken, saveAuthToken } from '@/logic/auth-logic';
 import { IUser } from '@/types';
-import { buildCookieOption } from '@/utils';
-import axios from 'axios';
-import { setCookie } from 'cookies-next';
 import { shallow } from 'zustand/shallow';
 import { createWithEqualityFn } from 'zustand/traditional';
 
@@ -21,13 +19,11 @@ export const useLoginStore = createWithEqualityFn<IAuthState>(
 );
 
 export const saveCredentials = ({ user, token }: { user: IUser; token: string }) => {
-  setCookie('access_token', token, buildCookieOption(30));
-  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  saveAuthToken(token);
   return useLoginStore.setState(() => ({ user, isAuth: true, isAdmin: user.isAdmin }));
 };
 
 export const clearCredentials = () => {
-  setCookie('access_token', '', buildCookieOption(0));
-  axios.defaults.headers.common.Authorization = '';
+  clearAuthToken();
   return useLoginStore.setState(() => ({ user: undefined, isAuth: false, isAdmin: false }));
 };
